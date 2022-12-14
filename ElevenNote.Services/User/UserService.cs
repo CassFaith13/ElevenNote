23ecea38f62a3ca12ac55a1b3c589c118dafefdd
CassFaith13 <cassandra.faith@icloud.com>
2022-12-13 20:20:10 -0500

    encryption

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,7 +1,54 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            if (await GetUserByEmailAsync(model.Email) != null || await GetUserByUsername(model.Username) != null)
+            {
+                return false;
+            }
+
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+
+            var passwordHasher = new PasswordHasher<UserEntity>();
+
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
+
+            _context.Users.Add(entity);
+
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
+
+
+
+        private async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+        }
+
+        private async Task<UserEntity> GetUserByUsername(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
+        }
     }
 }
--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using ElevenNote.Models.User;
 using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,24 @@ namespace ElevenNote.WebAPI.Controllers
         public UserController(IUserService service)
         {
             _service = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromForm] UserRegister newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _service.RegisterUserAsync(newUser);
+
+            if (registerResult)
+            {
+                return Ok("User successfully registered.");
+            }
+
+            return BadRequest("User NOT registered.");
         }
     }
 }
